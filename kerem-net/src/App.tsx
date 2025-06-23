7d55f053c9b7a2787ba23702fd667a06cb992db5
liranmanzouri <liranman77@gmail.com>
2025-06-23 18:06:34 +0300

    Added a posts.json which saves the posts

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import Post from "./features/Post/post";
 import './app.css'
+import postsJson from './posts.json'
 
 function App() {
-    const username: string = 'Liran';
-    const likes: number = 6;
-    const uploadDate: Date = new Date();
-    const text: string = 'This is my text!\n\nLorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus' +
-        ' blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique' +
-        ' volutpat consectetur congue lorem pharetra habitant. Sodales gravida egestas venenatis dignissim' +
-        ' molestie cursus porta. Massa lacus pulvinar aliquam mi tristique.\n\n' +
-        'Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras porttitor condimentum' +
-        ' orci suscipit. Leo maecenas in tristique, himenaeos elementum placerat. Taciti rutrum nostra,' +
-        ' eget cursus velit ultricies. Quam molestie tellus himenaeos cubilia congue vivamus ultricies.' +
-        ' Interdum praesent ut penatibus fames eros ad consectetur sed.';
 
-    const comments: string[] = ['wow', 'perfect', 'perfect', 'perfect', 'perfect', 'perfect'];
+    const posts = postsJson.posts;
 
     return (
         <div className={'app'}>
-            <Post username={username}
-                  likes={likes}
-                  uploadTime={uploadDate}
-                  text={text}
-                  comments={comments}></Post>
+            {
+                posts.map(post =>
+                    <Post username={post.username}
+                          likes={post.likes}
+                          uploadTime={new Date(post.uploadDate)}
+                          text={post.text}
+                          comments={post.comments}></Post>)
+            }
         </div>
     );
 }
