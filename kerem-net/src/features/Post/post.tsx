2bf38c0b2e51344bda90d5200f09fd494bd1c20b
liranmanzouri <liranman77@gmail.com>
2025-06-23 16:57:58 +0300

    Added post component and design

--- kerem-net/src/features/Post/post.tsx
+++ kerem-net/src/features/Post/post.tsx
@@ -1,4 +1,6 @@
 import React, {FC} from "react";
+import './post.css'
+import like from "../../icons/like.svg"
 
 interface Props {
     username: string;
@@ -23,15 +25,24 @@ const Post: FC<Props> = (
 
             <div className={'post-creator'}>
                 <span>{username}</span>
-                <span>{likes}</span>
-                <span>{uploadTime.toLocaleDateString()}</span>
+                <span className={'like'}>
+                    <span>{likes}</span>
+                    <img src={like} className="like-icon" alt="like-icon"/>
+                </span>
+                <div className={'date'}>
+                    <span>{uploadTime.toLocaleTimeString([], {timeStyle: 'short'})}</span>
+                    <span>{uploadTime.toLocaleDateString()}</span>
+                </div>
             </div>
 
-            <div className={'post-text'}>
-                {text}
+            <div className={'text-wrapper'}>
+                <div className={'post-text'}>
+                    {text.split('\n').map(line => <span>{line}<br/></span>)}
+                </div>
             </div>
 
             <div className={'post-comments'}>
+                <span>Comments:<br/></span>
                 <ul>
                     {comments.map(comment => <li className={'comment'}>{comment}</li>)}
                 </ul>
