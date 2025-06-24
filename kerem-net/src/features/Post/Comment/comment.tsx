8f71c2ca2d0312c6a1974d85fa2de7d4a39c36a5
liranmanzouri <liranman77@gmail.com>
2025-06-24 16:11:48 +0300

    Changed span tags with Typography component

--- kerem-net/src/features/Post/Comment/comment.tsx
+++ kerem-net/src/features/Post/Comment/comment.tsx
@@ -2,6 +2,7 @@ import React, {FC} from "react";
 import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
 import './comment.css';
 import CommentModel from "./comment-model";
+import {Typography} from "@mui/material";
 
 const Comment: FC<Omit<CommentModel, "id">> = ({content, publishDate, creator}) => {
     const date = new Date(publishDate);
@@ -12,10 +13,10 @@ const Comment: FC<Omit<CommentModel, "id">> = ({content, publishDate, creator}) 
             <CommentOutlinedIcon className={'comment-icon'}/>
             <div className={'comment-details'}>
                 <div className={'comment-publish-info'}>
-                    <span>{creator}</span>
-                    <span>{dateToShow}</span>
+                    <Typography className={'creator'}>{creator}</Typography>
+                    <Typography className={'publish-date'}>{dateToShow}</Typography>
                 </div>
-                <span className={'comment-content'}>{content}</span>
+                <Typography className={'comment-content'}>{content}</Typography>
             </div>
         </div>
     );
