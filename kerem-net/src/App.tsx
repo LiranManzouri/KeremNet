031d273a3900b5a63b1c5989e0ec9510db817bed
liranmanzouri <liranman77@gmail.com>
2025-06-26 11:04:54 +0300

    Added routes for each page

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './app.css';
-import SideBar from "./features/application-layout/side-bar";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+import Layout from "./features/application-layout/layout";
 import HomePage from "./features/home-page/home-page";
+import useGetPosts from "./features/home-page/useGetPosts";
+import SinglePostPage from "./features/single-post-page/single-post-page"
+import LoadingPage from "./features/loading-page/loading-page";
 
 function App() {
+    const posts = useGetPosts();
+
+    if (posts.length === 0) {
+        return (
+            <LoadingPage/>
+        );
+    }
+
     return (
-        <>
-            <SideBar/>
-            <HomePage/>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route index element={<Navigate to="posts"/>}/>
+                    <Route path={'posts'} element={<HomePage posts={posts}/>}/>
+                    <Route path={'posts/:postId'} element={<SinglePostPage posts={posts}/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
