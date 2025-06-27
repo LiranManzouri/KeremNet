6741ddaff715260c5cb8f41c3539811efc324bd1
liranmanzouri <liranman77@gmail.com>
2025-06-27 12:22:20 +0300

    Added a user login and register functionalities in server

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import postRouter from './routes/posts-route'
+import postsRouter from './routes/posts-route'
+import loginRouter from './routes/login-route'
 
 const app = express();
 
@@ -9,7 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-app.use('/posts', postRouter);
+app.use('/posts', postsRouter);
+app.use('/login', loginRouter);
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000!");
